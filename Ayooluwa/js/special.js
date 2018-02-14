4ed15379beb9aa65a665f6d507d627a5996cb6d3
oyewoas <31412106+oyewoas@users.noreply.github.com>
2018-02-14 03:57:26 +0100

    Add files via upload

---
+++ Ayooluwa/js/special.js
@@ -0,0 +1,36 @@
+// window.onscroll = function() { myFunction() };
+
+// var myNavbar = document.getElementById("bar");
+
+// var sticky = myNavbar.offsetTop;
+
+// function myFunction() {
+//     if (window.pageXOffset >= sticky) {
+
+//         bar.classList.add("sticky")
+//     } else {
+
+//         bar.classList.remove("sticky");
+//     }
+// }
+
+// var modal = document.getElementById('myModal');
+
+// var img = document.getElementById('imgmodal');
+
+// var modalImg = document.getElementById("modalimg");
+
+// var captionText = document.getElementById("modalcaption");
+
+// img.onclick = function() {
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+
+//     captionText.innerHTML = this.alt;
+// }
+
+// var span = document.getElementsByClassName("close")[0];
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
